f405d41522f0d6e509f9bf2d87eb7cf8ec647e51
Leo <leo.minorui@gmail.com>
2024-09-02 20:33:07 +0800

    add sort for insert、bubble、select

--- sort/sort.go
+++ sort/sort.go
@@ -1,4 +1,4 @@
-package golang
+package sort
 
 // 选择排序实现
 // 开启一个循环，每轮从未排序区间选择最小的元素，将其放到已排序区间的末尾
@@ -8,7 +8,7 @@ func SelectSort(arr []int) []int {
 		k := i
 		// 找到未排序区间内最小的一个元素，把该元素放到已排序区间最后
 		for j := i + 1; j < len(arr); j++ {
-			if arr[i] > arr[j] {
+			if arr[j] < arr[k] {
 				k = j
 			}
 		}
@@ -22,11 +22,35 @@ func SelectSort(arr []int) []int {
 // 直到每轮中把该轮循环中最大的数放到该轮的最右边
 
 func BubbleSort(arr []int) []int {
-	for i := 0; i < len(arr)-1; i++ {
-		for j := 0; j < len(arr)-i-1; j++ {
+	// 未排序区间 [0 i]
+	for i := len(arr) - 1; i > 0; i-- {
+		flag := false // 初始化标志位
+		// 在未排序区间内依次比较相邻元素
+		for j := 0; j < i; j++ {
 			if arr[j+1] < arr[j] {
 				arr[j+1], arr[j] = arr[j], arr[j+1]
+				flag = true
 			}
+		}
+		if flag == false {
+			break
+		}
+	}
+	return arr
+}
+
+// 插入排序实现
+// 默认数组一个元素已经排好序，剩余其他元素依次作为base元素根据大小排序
+// 插入到已排序区间内
+
+func InsertSort(arr []int) []int {
+	// 已排序区间设为 [0, i-1]
+	for i := 1; i < len(arr); i++ {
+		// 设置base元素
+		base := arr[i]
+		// 依次从右到左比较已排序区间元素
+		for j := i; j > 0 && arr[j-1] > base; j-- {
+			arr[j] = arr[j-1]
 		}
 	}
 	return arr
